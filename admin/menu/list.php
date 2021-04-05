804569f0d2868d61c98025459966f83b964a5be0
aboalfazlsh <69946439+aboalfazlsh@users.noreply.github.com>
2021-04-05 13:37:57 +0430

    Add files via upload

---
+++ admin/menu/list.php
@@ -0,0 +1,32 @@
+<div class="row">
+    <div class="col-lg-12">
+        <section class="panel">
+            <header class="panel-heading">
+                لیست منو های اصلی وب سایت
+            </header>
+            <table class="table table-striped table-advance table-hover">
+                <thead>
+                <tr>
+                    <th> نام محصول</th>
+                    <th> دسته بندی</th>
+                    <th>تصویر </th>
+                    <th>ویرایش</th>
+                    <th>حذف</th>
+                </tr>
+                </thead>
+                <tbody>
+
+                    <tr>
+                        <td>1</td>
+                        <td>2</td>
+                        <td><img width="60"  src=""/></td>
+                        <td><a href="dashbord.php?m=product&p=edit&id=" class="btn btn-primary btn-xs"><i class="icon-pencil"></i></a></td>
+                        <td><a href="dashbord.php?m=product&p=delete&id=" class="btn btn-danger btn-xs"><i class="icon-trash "></i></a></td>
+                    </tr>
+
+
+                </tbody>
+            </table>
+        </section>
+    </div>
+</div>
